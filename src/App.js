20be154ebb77db80527803242d9c0d686c7b80f6
Jaskaran Singh <jaskaran1523@gmail.com>
2022-02-13 02:28:05 -0800

    Make pages full height

--- src/App.js
+++ src/App.js
@@ -1,7 +1,16 @@
 import "./App.scss";
 
 function App() {
-  return <div className="App">Hello, world!</div>;
+  return (
+    <div className="App">
+      <div className="page home">Home</div>
+      <div className="page projects">Projects</div>
+      <div className="page about">About Me</div>
+      <div className="page skills">Skills</div>
+      <div className="page contact">Contact Me</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
 }
 
 export default App;
