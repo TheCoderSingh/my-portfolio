d9c3602cf275225d43797008df28b49486c9f206
Jaskaran Singh <jaskaran1523@gmail.com>
2022-02-13 02:41:19 -0800

    Create pages

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
+import Home from "./pages/Home/Home";
+import Projects from "./pages/Projects/Projects";
+import About from "./pages/About/About";
+import Skills from "./pages/Skills/Skills";
+import Contact from "./pages/Contact/Contact";
 import "./App.scss";
 
 function App() {
   return (
     <div className="App">
-      <div className="page home">Home</div>
-      <div className="page projects">Projects</div>
-      <div className="page about">About Me</div>
-      <div className="page skills">Skills</div>
-      <div className="page contact">Contact Me</div>
+      <div className="page">
+        <Home />
+      </div>
+      <div className="page">
+        <Projects />
+      </div>
+      <div className="page">
+        <About />
+      </div>
+      <div className="page">
+        <Skills />
+      </div>
+      <div className="page">
+        <Contact />
+      </div>
       <div className="footer">Footer</div>
     </div>
   );
