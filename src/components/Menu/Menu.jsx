9a77e3e5313786eb4f51d29e1ec9126d63790d39
Jaskaran Singh <jaskaran1523@gmail.com>
2022-02-13 19:59:35 -0800

    Install react-icons, add hamburger icon

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,9 +1,10 @@
+import { FaBars } from "react-icons/fa";
 import "./Menu.scss";
 
 const Menu = () => {
   return (
     <div className="menu">
-      <div>X</div>
+      <FaBars />
     </div>
   );
 };
