37267c080c3ba77fc95491bfb3dd61ae0ebf4c59
Jaskaran Singh <jaskaran1523@gmail.com>
2022-02-13 21:26:14 -0800

    Add menu items

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -2,20 +2,32 @@ import { useState } from "react";
 import "./Menu.scss";
 
 const Menu = () => {
-  const [isActive, setActive] = useState("false");
+  const [isActive, setActive] = useState(false);
 
   const handleClick = () => {
     setActive(!isActive);
   };
 
   return (
-    <div className={`menu ${!isActive ? "open" : null}`} onClick={handleClick}>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
-      <span></span>
+    <div>
+      <div className={`menu ${isActive ? "open" : null}`} onClick={handleClick}>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+        <span></span>
+      </div>
+      <div
+        className="menu-items"
+        style={{ display: isActive ? "block" : "none" }}
+      >
+        <div className="menu-item">Home</div>
+        <div className="menu-item">Projects</div>
+        <div className="menu-item">About</div>
+        <div className="menu-item">Skills</div>
+        <div className="menu-item">Contact</div>
+      </div>
     </div>
   );
 };
