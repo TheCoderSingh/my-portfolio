fda89054dfb4234b5f0504be2ab18e616b8cdc78
Jaskaran Singh <jaskaran1523@gmail.com>
2022-02-13 20:46:48 -0800

    Add animation to menu icon

--- src/components/Menu/Menu.jsx
+++ src/components/Menu/Menu.jsx
@@ -1,10 +1,21 @@
-import { FaBars } from "react-icons/fa";
+import { useState } from "react";
 import "./Menu.scss";
 
 const Menu = () => {
+  const [isActive, setActive] = useState("false");
+
+  const handleClick = () => {
+    setActive(!isActive);
+  };
+
   return (
-    <div className="menu">
-      <FaBars />
+    <div className={`menu ${!isActive ? "open" : null}`} onClick={handleClick}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
     </div>
   );
 };
